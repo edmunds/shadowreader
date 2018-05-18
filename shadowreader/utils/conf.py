d17ef1269acd7d64d37bb512eade17344688f1c8
Yuki Sawa <ysawa@edmunds.com>
2018-05-17 18:29:06 -0700

    Refactor config loader to add support for variable timeouts. Add comments

--- shadowreader/utils/conf.py
+++ shadowreader/utils/conf.py
@@ -55,66 +55,31 @@ def iter_namespace(ns_pkg):
 
 class Plugins:
     def __init__(self):
-        self.sr_config = load_yml_config(file='shadowreader.yml', key='config')
-        self.env_vars = self._init_env_vars(self.sr_config)
+        """ Class for reading and parsing shadowreader.yml to
+            load all specified plugins and store configurations
+        """
+        sr_config = load_yml_config(file='shadowreader.yml', key='config')
+        self.env_vars = self._init_env_vars(sr_config)
+        self.sr_config = self._identify_keys_w_stage(sr_config,
+                                                     self.env_vars['stage'])
 
-        plugins_location = self.sr_config['plugins_location']
-        plugins = importlib.import_module(plugins_location)
-
-        plugins_conf = load_yml_config(file='shadowreader.yml', key='plugins')
-
-        stage = self.env_vars['stage']
-
-        plugin_files = {
-            name: name
-            for finder, name, ispkg in iter_namespace(plugins)
-        }
-
-        self.plugins_conf = self._parse_plugins_conf(
-            plugins_conf=plugins_conf,
-            stage=stage,
-            plugins_location=plugins_location)
-
-        try:
-            sr_plugs = defaultdict(str)
-            for key, val in self.plugins_conf.items():
-                sr_plugs[key] = plugin_files[val]
-        except KeyError as e:
-            raise ImportError(
-                f'Failed to import plugin: \'{key}\', while looking for module: {e}'
-            )
-        self.sr_plugs = sr_plugs
-
-    def _parse_plugins_conf(self, *, plugins_conf: dict, stage: str,
-                            plugins_location: str):
-
-        plugins_conf = self._identify_plugins_w_stage(
-            plugins_conf=plugins_conf, stage=stage)
-
-        plugins_conf = {
-            key: f'{plugins_location}.{val}'
-            for key, val in plugins_conf.items()
-        }
-        return plugins_conf
-
-    def _identify_plugins_w_stage(self, *, plugins_conf: dict, stage: str):
-        for plugin, val in plugins_conf.items():
-            if 'stage' in val:
-                plugins_conf[plugin] = val['stage'][stage]
-        return plugins_conf
+        self._sr_plugins = self._load_plugins()
 
     def exists(self, plugin_name: str) -> bool:
-        if plugin_name in self.sr_plugs:
+        """ Check if a particular plugin was specified in the config"""
+        if plugin_name in self._sr_plugins:
             return True
         else:
             return False
 
     def load(self, plugin_name: str):
-        plugin_location = self.sr_plugs[plugin_name]
+        """ Load then return the plugin module"""
+        plugin_location = self._sr_plugins[plugin_name]
         plugin = importlib.import_module(plugin_location)
         return plugin
 
     def _init_env_vars(self, sr_config):
+        """ Read the Lambda Environment variables and store it """
         env_vars_to_get = sr_config['env_vars_to_get']
         env_vars = {}
         for env_var in env_vars_to_get:
@@ -125,8 +90,52 @@ class Plugins:
             if env_var in important_env_vars and not val:
                 msg = f'Invalid Lambda environment variable detected. env_var: {env_var}, env var val: {val}'
                 raise InvalidLambdaEnvVarError(msg)
-
         return env_vars
+
+    def _identify_keys_w_stage(self, conf: dict, stage: str):
+        """ If the config key specifies a key, then map the value based on the stage SR is deployed in"""
+        for key, val in conf.items():
+            if isinstance(val, dict) and 'stage' in val:
+                conf[key] = val['stage'][stage]
+        return conf
+
+    def _load_plugins(self):
+        """ Given the plugins_location, load in the modules found there based on the config file """
+        plugins_location = self.sr_config['plugins_location']
+        plugins = importlib.import_module(plugins_location)
+
+        plugin_files = {
+            name: name
+            for finder, name, ispkg in iter_namespace(plugins)
+        }
+
+        stage = self.env_vars['stage']
+
+        plugins_conf = load_yml_config(file='shadowreader.yml', key='plugins')
+        plugins_conf = self._parse_plugins(plugins_conf, stage,
+                                           plugins_location)
+
+        try:
+            sr_plugins = defaultdict(str)
+            for key, val in plugins_conf.items():
+                sr_plugins[key] = plugin_files[val]
+        except KeyError as e:
+            raise ImportError(
+                f'Failed to import plugin: \'{key}\', while looking for module: {e}'
+            )
+        return sr_plugins
+
+    def _parse_plugins(self, plugins_conf: dict, stage: str,
+                       plugins_location: str):
+        """ Parse the plugins config by mapping plugin names to module paths """
+        plugins_conf = self._identify_keys_w_stage(
+            conf=plugins_conf, stage=stage)
+
+        plugins_conf = {
+            key: f'{plugins_location}.{val}'
+            for key, val in plugins_conf.items()
+        }
+        return plugins_conf
 
 
 sr_plugins = Plugins()
